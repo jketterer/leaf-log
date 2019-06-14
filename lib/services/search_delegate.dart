c346300f80ae86ec2953eafb53fb8db435ded594
Jeremy Ketterer <j.ketterer1314@gmail.com>
2019-06-14 14:11:46 -0400

    modified search to display tea cards

--- lib/services/search_delegate.dart
+++ lib/services/search_delegate.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:leaf_log/models/tea.dart';
+import 'package:leaf_log/screens/tea_screens.dart';
+import 'package:leaf_log/screens/tea_widgets.dart';
 import 'package:leaf_log/services/database_helper.dart';
 
 class CustomSearchDelegate extends SearchDelegate {
   List<Tea> teaList;
   DatabaseHelper helper = DatabaseHelper.instance;
 
-  CustomSearchDelegate(
-
-  );
+  CustomSearchDelegate();
 
   @override
   ThemeData appBarTheme(BuildContext context) {
@@ -20,7 +20,6 @@ class CustomSearchDelegate extends SearchDelegate {
 
   @override
   List<Widget> buildActions(BuildContext context) {
-    // TODO: implement buildActions
     return [
       IconButton(
         icon: Icon(Icons.clear),
@@ -33,7 +32,6 @@ class CustomSearchDelegate extends SearchDelegate {
 
   @override
   Widget buildLeading(BuildContext context) {
-    // TODO: implement buildLeading
     return IconButton(
       icon: Icon(Icons.arrow_back),
       onPressed: () {
@@ -51,8 +49,6 @@ class CustomSearchDelegate extends SearchDelegate {
   @override
   Widget buildSuggestions(BuildContext context) {
     // TODO: implement buildSuggestions
-    List<String> test = ["test1", "test2", "test3"];
-
     return FutureBuilder<List<Tea>>(
       future: helper.searchTeaList(query),
       builder: (context, snapshot) {
@@ -60,11 +56,11 @@ class CustomSearchDelegate extends SearchDelegate {
         var list = snapshot.data;
         return snapshot.hasData
             ? new ListView.builder(
-              itemCount: list.length,
-              itemBuilder: (BuildContext context, int index) {
-                return new Text(list[index].name);
-              },
-            )
+                itemCount: list.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return new TeaCard(thisTea: list[index],);
+                },
+              )
             : new Center(child: new CircularProgressIndicator());
       },
     );

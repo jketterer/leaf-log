c30ba1c4d8cd8cab3aa3a916007c6948f5c152d8
jketterer <dev.jketterer@gmail.com>
2026-02-11 10:49:37 -0500

    feat: update color theme

--- composeApp/src/commonMain/kotlin/dev/jketterer/leaflog/presentation/ui/theme/Color.kt
+++ composeApp/src/commonMain/kotlin/dev/jketterer/leaflog/presentation/ui/theme/Color.kt
@@ -2,6 +2,7 @@ package dev.jketterer.leaflog.presentation.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+// Light Theme Colors
 val md_theme_light_primary = Color(0xFF4C662B)
 val md_theme_light_onPrimary = Color(0xFFFFFFFF)
 val md_theme_light_primaryContainer = Color(0xFFCCEDA3)
@@ -12,6 +13,39 @@ val md_theme_light_onSecondary = Color(0xFFFFFFFF)
 val md_theme_light_secondaryContainer = Color(0xFFDDE7C7)
 val md_theme_light_onSecondaryContainer = Color(0xFF171E0A)
 
+val md_theme_light_tertiary = Color(0xFF7D5635)
+val md_theme_light_onTertiary = Color(0xFFFFFFFF)
+val md_theme_light_tertiaryContainer = Color(0xFFFFDCC2)
+val md_theme_light_onTertiaryContainer = Color(0xFF2E1500)
+
+val md_theme_light_error = Color(0xFFBA1A1A)
+val md_theme_light_onError = Color(0xFFFFFFFF)
+val md_theme_light_errorContainer = Color(0xFFFFDAD6)
+val md_theme_light_onErrorContainer = Color(0xFF410002)
+
+val md_theme_light_background = Color(0xFFFEFCF5)
+val md_theme_light_onBackground = Color(0xFF1B1C18)
+val md_theme_light_surface = Color(0xFFFEFCF5)
+val md_theme_light_onSurface = Color(0xFF1B1C18)
+
+val md_theme_light_surfaceVariant = Color(0xFFE1E4D4)
+val md_theme_light_onSurfaceVariant = Color(0xFF44483D)
+val md_theme_light_outline = Color(0xFF75796C)
+val md_theme_light_outlineVariant = Color(0xFFC5C8B9)
+
+val md_theme_light_scrim = Color(0xFF000000)
+val md_theme_light_inverseSurface = Color(0xFF30312C)
+val md_theme_light_inverseOnSurface = Color(0xFFF2F1E9)
+val md_theme_light_inversePrimary = Color(0xFFB0D18A)
+
+val md_theme_light_surfaceTint = Color(0xFF4C662B)
+val md_theme_light_surfaceContainer = Color(0xFFEEEFE4)
+val md_theme_light_surfaceContainerHigh = Color(0xFFE8E9DE)
+val md_theme_light_surfaceContainerHighest = Color(0xFFE2E3D9)
+val md_theme_light_surfaceContainerLow = Color(0xFFF4F4EA)
+val md_theme_light_surfaceContainerLowest = Color(0xFFFFFFFF)
+
+// Dark Theme Colors
 val md_theme_dark_primary = Color(0xFFB0D18A)
 val md_theme_dark_onPrimary = Color(0xFF203600)
 val md_theme_dark_primaryContainer = Color(0xFF364E16)
@@ -21,3 +55,35 @@ val md_theme_dark_secondary = Color(0xFFC1CAAB)
 val md_theme_dark_onSecondary = Color(0xFF2B331D)
 val md_theme_dark_secondaryContainer = Color(0xFF414A32)
 val md_theme_dark_onSecondaryContainer = Color(0xFFDDE7C7)
+
+val md_theme_dark_tertiary = Color(0xFFEFBD94)
+val md_theme_dark_onTertiary = Color(0xFF48290D)
+val md_theme_dark_tertiaryContainer = Color(0xFF623F20)
+val md_theme_dark_onTertiaryContainer = Color(0xFFFFDCC2)
+
+val md_theme_dark_error = Color(0xFFFFB4AB)
+val md_theme_dark_onError = Color(0xFF690005)
+val md_theme_dark_errorContainer = Color(0xFF93000A)
+val md_theme_dark_onErrorContainer = Color(0xFFFFDAD6)
+
+val md_theme_dark_background = Color(0xFF1B1C18)
+val md_theme_dark_onBackground = Color(0xFFE4E3DB)
+val md_theme_dark_surface = Color(0xFF1B1C18)
+val md_theme_dark_onSurface = Color(0xFFE4E3DB)
+
+val md_theme_dark_surfaceVariant = Color(0xFF44483D)
+val md_theme_dark_onSurfaceVariant = Color(0xFFC5C8B9)
+val md_theme_dark_outline = Color(0xFF8E9285)
+val md_theme_dark_outlineVariant = Color(0xFF44483D)
+
+val md_theme_dark_scrim = Color(0xFF000000)
+val md_theme_dark_inverseSurface = Color(0xFFE4E3DB)
+val md_theme_dark_inverseOnSurface = Color(0xFF30312C)
+val md_theme_dark_inversePrimary = Color(0xFF4C662B)
+
+val md_theme_dark_surfaceTint = Color(0xFFB0D18A)
+val md_theme_dark_surfaceContainer = Color(0xFF2F302B)
+val md_theme_dark_surfaceContainerHigh = Color(0xFF3A3B35)
+val md_theme_dark_surfaceContainerHighest = Color(0xFF454640)
+val md_theme_dark_surfaceContainerLow = Color(0xFF242520)
+val md_theme_dark_surfaceContainerLowest = Color(0xFF0E0F0B)

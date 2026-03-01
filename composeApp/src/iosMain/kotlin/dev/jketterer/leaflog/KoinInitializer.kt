ebab7b9eb1813621aa31bed431662e8b263d7aca
jketterer <dev.jketterer@gmail.com>
2026-03-01 12:51:58 -0500

    fix: populate defaults on iOS startup

--- composeApp/src/iosMain/kotlin/dev/jketterer/leaflog/KoinInitializer.kt
+++ composeApp/src/iosMain/kotlin/dev/jketterer/leaflog/KoinInitializer.kt
@@ -4,7 +4,11 @@ import dev.jketterer.leaflog.di.appModule
 import dev.jketterer.leaflog.di.platformModule
 import dev.jketterer.leaflog.di.repositoryModule
 import dev.jketterer.leaflog.di.useCaseModule
+import dev.jketterer.leaflog.domain.usecases.InitializeDefaultsUseCase
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
 import org.koin.core.context.startKoin
+import org.koin.mp.KoinPlatform
 
 fun initKoin() {
     startKoin {
@@ -14,5 +18,9 @@ fun initKoin() {
             repositoryModule,
             useCaseModule,
         )
+    }
+
+    MainScope().launch {
+        KoinPlatform.getKoin().get<InitializeDefaultsUseCase>().invoke()
     }
 }

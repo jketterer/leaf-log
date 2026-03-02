92e8a8b829e8b4533e3508cf3b879864731217e6
jketterer <dev.jketterer@gmail.com>
2026-03-02 09:27:51 -0500

    fix: allow tap-to-dismiss keyboard behavior globally

--- composeApp/src/commonMain/kotlin/dev/jketterer/leaflog/App.kt
+++ composeApp/src/commonMain/kotlin/dev/jketterer/leaflog/App.kt
@@ -1,6 +1,12 @@
 package dev.jketterer.leaflog
 
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.tooling.preview.Preview
 import dev.jketterer.leaflog.presentation.ui.navigation.AppNavigation
 import dev.jketterer.leaflog.presentation.ui.theme.LeafLogTheme
@@ -9,6 +15,15 @@ import dev.jketterer.leaflog.presentation.ui.theme.LeafLogTheme
 @Preview
 fun App() {
     LeafLogTheme {
-        AppNavigation()
+        val focusManager = LocalFocusManager.current
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .pointerInput(Unit) {
+                    detectTapGestures { focusManager.clearFocus() }
+                },
+        ) {
+            AppNavigation()
+        }
     }
 }

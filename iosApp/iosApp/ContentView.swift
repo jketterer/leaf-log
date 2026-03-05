69c1e85f3d2dc1039dd9e825925dc93bcb857551
jketterer <dev.jketterer@gmail.com>
2026-03-05 12:04:24 -0500

    fix: recreate iOS compose view on app resume

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -11,9 +11,18 @@ struct ComposeView: UIViewControllerRepresentable {
 }
 
 struct ContentView: View {
+    @Environment(\.scenePhase) private var scenePhase
+    @State private var renderKey = 0
+
     var body: some View {
         ComposeView()
             .ignoresSafeArea()
+            .id(renderKey)
+            .onChange(of: scenePhase) { phase in
+                if phase == .active {
+                    renderKey += 1
+                }
+            }
     }
 }
 

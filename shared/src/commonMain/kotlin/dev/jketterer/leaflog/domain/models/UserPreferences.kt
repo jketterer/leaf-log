b6c7dc9becfac8b1e4352381e0c84b8381eea465
jketterer <dev.jketterer@gmail.com>
2026-03-18 08:35:49 -0400

    chore: swap default units to imperial

--- shared/src/commonMain/kotlin/dev/jketterer/leaflog/domain/models/UserPreferences.kt
+++ shared/src/commonMain/kotlin/dev/jketterer/leaflog/domain/models/UserPreferences.kt
@@ -1,8 +1,8 @@
 package dev.jketterer.leaflog.domain.models
 
 data class UserPreferences(
-    val temperatureUnit: TemperatureUnit = TemperatureUnit.CELSIUS,
-    val volumeUnit: VolumeUnit = VolumeUnit.MILLILITERS,
+    val temperatureUnit: TemperatureUnit = TemperatureUnit.FAHRENHEIT,
+    val volumeUnit: VolumeUnit = VolumeUnit.FLUID_OUNCES,
     val teaSortOption: TeaSortOption = TeaSortOption.NAME_ASC,
     val analyticsPeriod: AnalyticsPeriod = AnalyticsPeriod.THIS_WEEK,
     val defaultWaterType: WaterType = WaterType.FILTERED,
